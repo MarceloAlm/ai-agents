9cfaee96e03146dea4b311b2f377c210ee1930db
Marcelo de Almeida <marceloalm@outlook.com.br>
2026-09-11 21:46:53 -0300

    Corrige repasse de argumentos nos scripts de execução dos containers

--- opencode-dotnet/opencode-dotnet.sh
+++ opencode-dotnet/opencode-dotnet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run --rm -it --userns=keep-id -v "opencode-home:/home/node" -v "$PWD:/workspace" -w /workspace opencode:dotnet
+podman run --rm -it --userns=keep-id -v "opencode-home:/home/node" -v "$PWD:/workspace" -w /workspace opencode:dotnet opencode "$@"
